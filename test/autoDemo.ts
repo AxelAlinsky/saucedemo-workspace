65c517f800b2ccfdf72f1609a398a65bb447f4bd
Axel Alinsky <matthewkenn00@gmail.com>
2023-12-03 04:05:33 +0700

    Fixing npm run test

--- test/autoDemo.ts
+++ test/autoDemo.ts
@@ -15,62 +15,46 @@ describe('Web Automation', async function () {
   let driver: WebDriver;
 
   before(async function () {
-    driver = await new Builder().forBrowser('chrome').build();
-    ObjApp = new App(driver); // Pass the driver instance here
-    await driver.get('https://www.saucedemo.com'); // Navigate to the website
-
-    await driver.manage().window().maximize(); // Maximize the browser window
+      driver = await new Builder().forBrowser('chrome').build();
+      ObjApp = new App(driver); // Pass the driver instance here
+      await driver.get('https://www.saucedemo.com'); // Navigate to the website
+      await driver.manage().window().maximize(); // Maximize the browser window
   });
 
   after(async function () {
-    await driver.quit();
+      await driver.quit(); // Close the driver after each test case
   });
 
-  describe('Web Automation', async function () {
-    before(async function () {
+  it('Step 1: Open Browser', async function () {
+    await driver.sleep(1000);
 
-
-    });
-
-    afterEach(async function () {
-
-    });
-
-    after(async function () {
-      await driver.close();
-    });
-
-    it('Step 1: Open Browser', async function () {
-      await driver.sleep(1000);
-
-      await ObjApp.verifyTitle();
-    });
-
-    it('Step 2: Login to application with invalid credentials', async function () {
-      await ObjApp.insertText(By.id(compMain.userNameInput), 'invalid_user');
-
-      await ObjApp.insertText(By.id(compMain.userPassInput), '123456');
-
-      await ObjApp.click(By.id(compMain.loginButtion));
-    });
-
-    it('Step 3: Verify error message', async function () {
-      await ObjApp.verifyErrorMsg();
-
-      await ObjApp.click(By.css(compMain.errorMsgCloseBtn));
-    });
-
-    it('Step 4: Login to application with valid credentials', async function () {
-      await ObjApp.insertText(By.id(compMain.userNameInput), 'standard_user');
-
-      await ObjApp.insertText(By.id(compMain.userPassInput), 'secret_sauce');
-
-      await ObjApp.click(By.id(compMain.loginButtion));
-    });
-
-    it('Step 5: Verify Successful Login', async function () {
-      await ObjApp.verifyUrl(DataURL.saucedemoInventory);
-    });
+    await ObjApp.verifyTitle();
   });
 
+  it('Step 2: Login to application with invalid credentials', async function () {
+    await ObjApp.insertText(By.id(compMain.userNameInput), 'invalid_user');
+
+    await ObjApp.insertText(By.id(compMain.userPassInput), '123456');
+
+    await ObjApp.click(By.id(compMain.loginButtion));
+  });
+
+  it('Step 3: Verify error message', async function () {
+    await ObjApp.verifyErrorMsg();
+
+    await ObjApp.click(By.css(compMain.errorMsgCloseBtn));
+  });
+
+  it('Step 4: Login to application with valid credentials', async function () {
+    await ObjApp.insertText(By.id(compMain.userNameInput), 'standard_user');
+
+    await ObjApp.insertText(By.id(compMain.userPassInput), 'secret_sauce');
+
+    await ObjApp.click(By.id(compMain.loginButtion));
+  });
+
+  it('Step 5: Verify Successful Login', async function () {
+    await ObjApp.verifyUrl(DataURL.saucedemoInventory);
+  });
 });
+
